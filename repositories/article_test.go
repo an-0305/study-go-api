03802fae6643175240b218aa3fc7551121c7a5e4
akari nakamura <akarinakamura0305@gmail.com>
2023-02-01 10:05:03 +0900

    add TestSelectArticleList

--- repositories/article_test.go
+++ repositories/article_test.go
@@ -73,3 +73,26 @@ func TestSelectArticleDetail(t *testing.T) {
 		})
 	}
 }
+
+func TestSelectArticleList(t *testing.T) {
+	dbUser := "docker"
+	dbPassword := "docker"
+	dbDatabase := "sampledb"
+	dbConn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser, dbPassword, dbDatabase)
+
+	db, err := sql.Open("mysql", dbConn)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer db.Close()
+
+	expectedNum := 2
+	got , err := repositories.SelectArticleList(db, 1)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if num := len(got); num != expectedNum {
+		t.Errorf("want %d but got %d articles\n", expectedNum, num)
+	}
+}
