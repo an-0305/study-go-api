6b1353573eab9c0f6d43ea16c625a1ebdc2ddf57
akari nakamura <akarinakamura0305@gmail.com>
2023-02-17 15:44:20 +0900

    defining a custom error type

---
+++ apperrors/error.go
@@ -0,0 +1,16 @@
+package apperrors
+
+type MyAppError struct {
+	ErrCode
+	Message string
+	Err error
+}
+
+func (myErr *MyAppError) Error() string {
+	return myErr.Err.Error()
+}
+
+
+func (myErr *MyAppError) Unwrap() error {
+	return myErr.Err
+}
---
+++ apperrors/errorcode.go
@@ -0,0 +1,7 @@
+package apperrors
+
+type ErrCode string
+
+const (
+	Unkown ErrCode = "U000"
+)
