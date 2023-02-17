faa487ff27e548d26903b6ee0c05983848fdbba2
akari nakamura <akarinakamura0305@gmail.com>
2023-02-17 17:24:05 +0900

    add error handling to the service layer

--- apperrors/errorcode.go
+++ apperrors/errorcode.go
@@ -4,4 +4,10 @@ type ErrCode string
 
 const (
 	Unkown ErrCode = "U000"
+
+	InsertDataFailed ErrCode = "S001"
+	GetDataFailed ErrCode = "S002"
+	NAData ErrCode = "S003"
+	NoTartegData ErrCode = "S004"
+	UpdateDataFailed ErrCode = "S005"
 )
